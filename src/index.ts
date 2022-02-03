68e13275fab2c726dfd179ecfaf605210d658981
KiranMantha <kiranv.mantha@gmail.com>
2022-02-03 15:47:53 +0530

    fixed eslint

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,18 @@ import { Component, html } from '@plumejs/core';
 import styles from './styles/base.scss';
 
 @Component({
-    selector: 'app-root',
-    styles: styles
+  selector: 'app-root',
+  styles: styles,
+  root: true
 })
 export class AppComponent {
-    render() {
-        return html`
-            <main class='container center'>
-                <img src="./images/logo.jpg" />
-                <h1>Welcome to PlumeJS</h1>
-                <p>Please check <a href="https://github.com/KiranMantha/plumejs">here</a> for documentation</p>
-            </main>
-        `;
-    }
+  render() {
+    return html`
+      <main class="container center">
+        <img src="./images/logo.jpg" />
+        <h1>Welcome to PlumeJS</h1>
+        <p>Please check <a href="https://github.com/KiranMantha/plumejs">here</a> for documentation</p>
+      </main>
+    `;
+  }
 }
