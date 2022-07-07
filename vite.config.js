809075115caee4164b489a3a1d949a4a279a9fad
KiranMantha <kiranv.mantha@gmail.com>
2022-07-07 16:25:30 +0530

    WIP: moving to vitest from jest for better ESM support in unit tests

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,14 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'happy-dom'
+    environment: 'happy-dom',
+    deps: {
+      inline: true
+    },
+    coverage: {
+      reporter: ['text', 'json', 'html'], 
+      cleanOnRerun: true, 
+      reportsDirectory: 'coverage'
+    }
   }
 });
