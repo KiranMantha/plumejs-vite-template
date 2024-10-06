d2762acf3c2fbd289f0615aa15af12e23a825ed7
KiranMantha <kiranv.mantha@gmail.com>
2024-10-07 00:41:12 +0530

    adjusted unit tests and upgraded plumejs core version

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,18 @@ import { Component, html } from '@plumejs/core';
 import styles from './styles/base.scss?inline';
 
 @Component({
+  selector: 'test-ele'
+})
+class TestElement {
+  render() {
+    return html`<div data-testid="test-ele">
+      <p>i'm child element</p>
+      <p></p>
+    </div>`;
+  }
+}
+
+@Component({
   selector: 'app-root',
   styles: styles,
   root: true
@@ -14,6 +26,7 @@ export class AppComponent {
         <img src="./images/logo.jpg" />
         <h1>Welcome to PlumeJS</h1>
         <p>Please check <a href="https://github.com/KiranMantha/plumejs">here</a> for documentation</p>
+        <test-ele></test-ele>
       </main>
     `;
   }
